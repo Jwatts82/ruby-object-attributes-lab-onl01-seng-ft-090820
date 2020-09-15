b8a2fddb3d157c19aff94eb47b247c171f591609
Jessica Watts <jessperr182@hotmail.com>
2020-09-15 18:20:38 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def name(dog_name)
+    @this_is_dogs_name = dog_name
+  end
+  
+  def name
+    @this_is_dogs_name
+  end
+  
+  def breed(dog_breed)
+    @this_is_dog_breed = dog_breed
+  end
+  
+  def breed
+    @this_is_dog_breed
+  end
+    
+end 
